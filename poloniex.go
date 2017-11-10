f221e84deb2e6f788dd18fb7c877a9f60b9134d1
zcong1993 <1005997919@qq.com>
2017-11-10 18:09:36 +0800

    finished polo

--- poloniex.go
+++ poloniex.go
@@ -5,6 +5,8 @@ import (
 	"github.com/bitly/go-simplejson"
 	"github.com/mariuspass/recws"
 	"log"
+	"sort"
+	"strconv"
 	"time"
 )
 
@@ -14,6 +16,9 @@ const (
 	INIT_TYPE  = "i"
 	ORDER      = "o"
 	HISTORY    = "t"
+	REMOVE     = "0.00000000"
+	SELLSIDE   = "1"
+	BUYSIDE    = "1"
 )
 
 type Order struct {
@@ -21,11 +26,13 @@ type Order struct {
 	Quantity float64
 }
 
+type OrderArr []Order
+
 type Orderbook struct {
-	Bids      []Order
-	Asks      []Order
-	Version   float64
-	Timestamp time.Time
+	Bids      OrderArr
+	Asks      OrderArr
+	Version   string
+	Timestamp int64
 	IsValid   bool
 }
 
@@ -33,6 +40,8 @@ type Polo struct {
 	ws_url  string
 	rc      *recws.RecConn
 	cmds    []interface{}
+	bidsMap map[string]interface{}
+	asksMap map[string]interface{}
 	channel string
 	Orderbook
 }
@@ -49,6 +58,7 @@ func Interface2String(in interface{}) string {
 func NewPolo(channel string) *Polo {
 	polo := &Polo{ws_url: ws_url, channel: channel}
 	polo.Orderbook.IsValid = false
+	polo.Connect()
 	return polo
 }
 
@@ -65,7 +75,7 @@ func (p *Polo) Subscribe(channel string) {
 }
 
 func (p *Polo) Handler() {
-	p.Subscribe("BTC_BCH")
+	p.Subscribe(p.channel)
 	for {
 		_, message, err := p.rc.ReadMessage()
 		if err != nil {
@@ -83,16 +93,48 @@ func (p *Polo) Handler() {
 		code := Interface2String(arr[0])
 		if code == HEART_BEAT {
 			fmt.Println("heartbeat")
+		} else if len(arr) == 2 {
+			//
 		} else {
 			for _, book := range arr[2].([]interface{}) {
 				t := book.([]interface{})[0].(string)
 				switch t {
 				case INIT_TYPE:
 					// init
+					p.Orderbook.Version = Interface2String(arr[1])
 					data := book.([]interface{})[1].(map[string]interface{})
-					fmt.Println(data["currencyPair"])
+					orderbooks := data["orderBook"].([]interface{})
+					p.asksMap = orderbooks[0].(map[string]interface{})
+					p.bidsMap = orderbooks[1].(map[string]interface{})
+					p.updateOrderbook()
 				case ORDER:
 					// increase
+					pre, _ := strconv.ParseInt(p.Orderbook.Version, 10, 64)
+					new, _ := strconv.ParseInt(Interface2String(arr[1]), 10, 64)
+					if new != pre+1 {
+						p.Handler()
+						return
+					}
+					p.Orderbook.Version = Interface2String(arr[1])
+					data := book.([]interface{})
+					side := Interface2String(data[1])
+					price := Interface2String(data[2])
+					quantity := Interface2String(data[3])
+
+					if side == BUYSIDE {
+						if quantity == REMOVE {
+							delete(p.bidsMap, price)
+						} else {
+							p.bidsMap[price] = data[3]
+						}
+					} else if side == SELLSIDE {
+						if quantity == REMOVE {
+							delete(p.asksMap, price)
+						} else {
+							p.asksMap[price] = data[3]
+						}
+					}
+					p.updateOrderbook()
 				case HISTORY:
 					// history
 				}
@@ -101,7 +143,46 @@ func (p *Polo) Handler() {
 	}
 }
 
+func (s OrderArr) Len() int {
+	return len(s)
+}
+
+func (s OrderArr) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s OrderArr) Less(i, j int) bool {
+	return s[i].Price < s[j].Price
+}
+
+func (s OrderArr) Reverse() OrderArr {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
+
+func (p *Polo) updateOrderbook() {
+	tmp := []Order{}
+	for p, q := range p.asksMap {
+		fp, _ := strconv.ParseFloat(p, 64)
+		fq, _ := strconv.ParseFloat(Interface2String(q), 64)
+		tmp = append(tmp, Order{fp, fq})
+	}
+	sort.Sort(OrderArr(tmp))
+	p.Orderbook.Asks = tmp
+	tmp = []Order{}
+	for p, q := range p.bidsMap {
+		fp, _ := strconv.ParseFloat(p, 64)
+		fq, _ := strconv.ParseFloat(Interface2String(q), 64)
+		tmp = append(tmp, Order{fp, fq})
+	}
+	sort.Sort(OrderArr(tmp))
+	p.Orderbook.Bids = OrderArr(tmp).Reverse()
+	p.Orderbook.Timestamp = time.Now().Unix()
+	p.Orderbook.IsValid = true
+	fmt.Printf("%+v", p.Orderbook)
+}
+
 func main() {
-	polo := NewPolo("BTC_BCH")
-	polo.Connect()
+	NewPolo("BTC_BCH")
 }
